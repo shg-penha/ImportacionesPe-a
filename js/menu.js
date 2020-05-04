37ec381e5283d45dd5649d7c8bc3fa36e9cea5dd
shg-penha <62841245+shg-penha@users.noreply.github.com>
2020-05-04 14:29:18 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+
+
+// MENU
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('barra');
+
+btnMenu.addEventListener('click', function(){	
+	nav.classList.toggle('mostrar');
+})
+
+
+
